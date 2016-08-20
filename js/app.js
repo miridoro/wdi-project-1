72d3c8231b57d8053fc4b13d46e4abac53417474
miridoro <miriam.wodrich@gmail.com>
2016-08-20 13:55:15 +0100

    debugging2

--- js/app.js
+++ js/app.js
@@ -1,121 +1,118 @@
-
-
 $(init)
 
 function init() {
 
 
-  console.log("hello");
+    console.log("hello");
 
-  //setting up game object
+    //setting up game object
 
-  var game = {
-    bestScore: 0
-    
-  }
+    var game = {
+        bestScore: 0
 
-  
-  
-  
-  
-
-
-  //adding functions to game object
-
-  game.startNewGame = function(){
-    //resets all variables
-    // event.preventDefault();
-    game.computerSequence = [];
-    game.currentScore= 0;
-    game.gameOn= true;
-    game.round = 0;
-
-
-
-    // launch game
-    game.startNewRound();
-    
-  }
-
-
-  game.startNewRound= function() {
-    //resets variables except bestScore
-    console.log("startNewRound");
-    this.sequenceIndex= 0;
-    playTunes();
-
-    // game.computerSequence = [];
-    // game.sequenceIndex= 0;
-    this.round++;
-  }
-
-  
-
- 
-
-  game.processUserChoice= function(buttonId) {
-    
-    console.log("Button ID: " + buttonId + " computer id: " + game.computerSequence[game.sequenceIndex]);
-
-    if(buttonId == game.computerSequence[game.sequenceIndex]){
-    // sequenceIndex++;
-    console.log("You win!");
     }
 
-    if(game.sequenceIndex === game.computerSequence.length){
-      // console.log("You win!");
-     
-      game.currentScore++;
-      // console.log(game.currentScore);
+
+    //adding functions to game object
+
+    game.startNewGame = function() {
+        //resets all variables
+        // event.preventDefault();
+        console.log("startNewGame");
+
+        game.computerSequence = [];
+        game.currentScore = 0;
+        game.gameOn = true;
+        game.round = 0;
+
+
+
+        // launch game
+        game.startNewRound();
+
     }
-    else{
-      console.log("Try again.");
+
+
+    game.startNewRound = function() {
+        //resets variables except bestScore
+        console.log("startNewRound");
+
+        //this is the user index sequence
+        this.sequenceIndex = 0;
+        playTunes();
+
+        // game.computerSequence = [];
+        // game.sequenceIndex= 0;
+        this.round++;
     }
 
-    // startNewRound();
-  }
+
+    game.endGame = function() {
+        console.log("endGame");
+    }
 
 
-  //HTML (VIEW + CONTROLLER)
-  
-  function clickButtons(){
-    var buttonId = this.getAttribute("value");
-    game.processUserChoice(buttonId);
-    
-    // game.userSequence.push(buttonId);
-    // console.log("User clicked = " + game.userSequence);
-    console.log("User clicked = " + buttonId);
-  }
+    game.processUserChoice = function(buttonId) {
 
-  function playTunes() {
-    // event.preventDefault();
-    console.log("Play Tunes");
-    
-    randNum = (Math.floor((Math.random() * 4) + 1));
-    game.computerSequence.push(randNum);
-    console.log("Computer plays: " + game.computerSequence);
-  
+        console.log("Button ID: " + buttonId + " computer id: " + this.computerSequence[this.sequenceIndex]);
 
-    //play buttons (sounds and highlight colors)
-  }
+        if (buttonId == this.computerSequence[this.sequenceIndex]) {
 
 
 
+            if (this.sequenceIndex === this.computerSequence.length - 1) {
+                console.log("You win!");
+                this.currentScore++;
+                this.startNewRound();
+            } else {
+                this.sequenceIndex++;
+            }
+
+        } else {
+
+            this.endGame();
+        }
 
 
-  //START PROGRAM
+    }
 
-  //add eventlisteners
+    //HTML (VIEW + CONTROLLER)
 
-  $("#newRound").on("click", game.startNewRound);
-  $("li").on("click", clickButtons);
+    function clickButtons() {
+        var buttonId = this.getAttribute("value");
+        console.log("User clicked = " + buttonId);
 
-  game.startNewGame();
+        game.processUserChoice(buttonId);
+    }
+
+
+    function playTunes() {
+        // event.preventDefault();
+        console.log("Play Tunes");
+
+        var randNum = (Math.floor((Math.random() * 4) + 1));
+        game.computerSequence.push(randNum);
+        console.log("Computer plays: " + game.computerSequence);
+
+
+        //play buttons (sounds and highlight colors)
+    }
 
 
 
- 
+
+
+    //START PROGRAM
+
+    //add eventlisteners
+
+    $("#newRound").on("click", game.startNewRound);
+    $("li").on("click", clickButtons);
+
+    game.startNewGame();
+
+
+
+
 
 }
-
-
